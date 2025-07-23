c9027af4e19c5a1db9738084d92d27d2dd2a3a50
Modin <eom3658@gmail.com>
2025-07-23 16:32:30 +0900

    feat: FindPathWithDijkstar

--- MyPathFinder/MazePathFinder.cpp
+++ MyPathFinder/MazePathFinder.cpp
@@ -42,18 +42,17 @@ void MazePathFinder::FindPathWithDFS()
     /*
      * DFS
      *  1. 시작 지점을 방문하고 스택에 넣음
-     *  2. 스택에서 값을 꺼내 현재 위치로 설정
-     *  3. 아직 방문하지 않은 이웃 셀(상, 하, 좌, 우)를 확인
-     *  4-1. 방문 가능한 이웃이 있다면 그 셀을 방문 후 스택에 넣고 3단계로 회귀
-     *  4-2. 방문 가능한 이웃이 없다면 2단계로 회귀
-     *  4-3. 현재 위치가 도착지거나 스택이 비었으면 탐색 종료
+     *  2. 스택이 비거나 도착지에 도달할 때까지 다음 단계를 반복
+     *  3-1. 스택에서 값을 꺼내 현재 위치로 설정
+     *  3-2. 현재 위치를 기준으로 인접 셀을 확인
+     *  3-2. 인접 셀이 방문하지 않았고 벽이 아니라면 스택에 넣고 방문 처리
      */
 
     std::stack<std::pair<int, int>> stack;
-    bool visited[MazeConfig::WIDTH][MazeConfig::HEIGHT] = { false };
+    bool visited[MazeConfig::HEIGHT][MazeConfig::WIDTH] = { false };
 
-    std::pair<int, int> start = { 1, 1 };
-    std::pair<int, int> end = { MazeConfig::WIDTH - 2, MazeConfig::HEIGHT - 2 };
+    const std::pair<int, int> start = { 1, 1 };
+    const std::pair<int, int> end = { MazeConfig::WIDTH - 2, MazeConfig::HEIGHT - 2 };
     stack.push(start);
     
     visited[start.second][start.first] = true;
@@ -110,18 +109,17 @@ void MazePathFinder::FindPathWithBFS()
     /*
      * BFS
      *  1. 시작 지점을 방문하고 큐에 넣음
-     *  2. 큐에서 값을 꺼내 현재 위치로 설정
-     *  3. 아직 방문하지 않은 이웃 셀(상, 하, 좌, 우)를 확인
-     *  4. 방문 가능한 이웃 셀을 모두 큐에 넣고 방문 처리
-     *  5. 현재 위치가 도착지라면 탐색 종료
-     *  6. 큐가 빌 때까지 2단계로 회귀
+     *  2. 큐가 비거나 도착지에 도달할 때까지 다음 단계를 반복
+     *  3-1. 큐에서 값을 꺼내 현재 위치로 설정
+     *  3-2. 현재 위치를 기준으로 인접 셀을 확인
+     *  3-2. 인접 셀이 방문하지 않은 곳이며 벽이 아니라면 큐에 넣고 방문 처리
      */
 
     std::queue<std::pair<int, int>> queue;
     bool visited[MazeConfig::HEIGHT][MazeConfig::WIDTH] = { false };
 
-    std::pair<int, int> start = { 1, 1 };
-    std::pair<int, int> end = { MazeConfig::WIDTH - 2, MazeConfig::HEIGHT - 2 };
+    const std::pair<int, int> start = { 1, 1 };
+    const std::pair<int, int> end = { MazeConfig::WIDTH - 2, MazeConfig::HEIGHT - 2 };
     queue.push(start);
     
     visited[start.second][start.first] = true;
@@ -168,7 +166,77 @@ void MazePathFinder::FindPathWithBFS()
 
 void MazePathFinder::FindPathWithDijkstar()
 {
+    /*
+     * Dijkstra
+     *  1. 시작 지점의 거리를 0으로 설정, 나머지는 무한대로 초기화
+     *  2. 시작 지점을 거리가 짧은 순서로 처리하는 우선순위 큐에 넣음
+     *  3. 큐가 비거나 도착지에 도달할 때까지 다음 단계를 반복
+     *  4-1. 큐에서 가장 거리가 짧은 셀을 꺼내고, 해당 셀에 인접한 셀을 검사
+     *  4-2. 인접 셀이 방문하지 않았고 벽이 아니며, 현재 경로보다 더 짧은 거리가 발견되면 거리를 갱신한다.
+     */
     
+    int distance[MazeConfig::HEIGHT][MazeConfig::WIDTH];
+    bool visited[MazeConfig::HEIGHT][MazeConfig::WIDTH] = { false };
+    
+    std::fill(&distance[0][0], &distance[0][0] + MazeConfig::HEIGHT * MazeConfig::WIDTH, INT_MAX);
+    
+    const std::pair<int, int> start = { 1, 1 };
+    const std::pair<int, int> end = { MazeConfig::WIDTH - 2, MazeConfig::HEIGHT - 2 };
+    distance[start.second][start.first] = 0;
+
+    using Node = std::pair<int, std::pair<int, int>>;
+    std::priority_queue<Node, std::vector<Node>, std::greater<Node>> pq;
+    pq.push({0, start});
+
+    const int dx[4] = { 0, 0, -1, 1 };
+    const int dy[4] = { -1, 1, 0, 0 };
+
+    while (!pq.empty())
+    {
+        Node current = pq.top();
+        int dist = current.first;
+        std::pair<int, int> cell = current.second;
+        
+        pq.pop();
+
+        int x = cell.first;
+        int y = cell.second;
+
+        if (visited[y][x])
+            continue;
+
+        visited[y][x] = true;
+        m_maze[y][x] = 2;
+        m_mazeDrawer.Draw(m_maze);
+
+        if (cell == end)
+            break;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx <= 0 || ny <= 0)
+                continue;
+
+            if (nx >= MazeConfig::HEIGHT || ny >= MazeConfig::WIDTH)
+                continue;;
+            
+            if (visited[ny][nx])
+                continue;
+
+            if (m_maze[ny][nx] == 0)
+                continue;
+
+            int newDist = dist + 1;
+            if (newDist < distance[ny][nx])
+            {
+                distance[ny][nx] = newDist;
+                pq.push({newDist, {nx, ny}});
+            }
+        }
+    }
 }
 
 void MazePathFinder::FindPathWithAStar()
